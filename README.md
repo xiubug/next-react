3bb70b09b57e55763f2348ec12cb353c7b7bed1a
sosout <sosout@139.com>
2018-01-08 17:24:38 +0800

    项目初始化

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # next-react
+
+基于next.js + react + express 最佳实践
+
+- 预览：http://next.sosout.com/
+
+## 特性
+
+- :gem: **特性一**：服务器端渲染(默认)
+- :rocket: **特性二**：自动代码切分, 加速页面加载
+- :1234: **特性三**：简单的客户端路由(基于页面)
+- :gem: **特性四**：可以使用Express 或其他Node.js服务器实现
+- :rocket: **特性五**：基于Webpack的开发环境, 支持热模块替换(HMR: Hot Module Replacement)
+- :1234: **特性六**：使用Babel和Webpack配置定制
+
+## 模板
+- [ ] 项目搭建
+
+## 使用
+
+```bash
+$ git clone https://github.com/sosout/next-react.git
+$ cd next-react
+$ npm install
+$ npm run dev         # 访问 http://localhost:3002
+```
---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Layout from '../components/MyLayout'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const Index = (props) => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      { props.shows.map(({ show }) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      )) }
+    </ul>
+  </Layout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('http://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  return {
+    shows: data
+  }
+}
+
+export default Index
